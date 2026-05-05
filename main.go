3b615a3a10b7b744595fec336f1a3f956e47a1cc
Yeonuk Hwang <charlie.hwang131@gmail.com>
2026-05-05 17:03:46 +0930

    create hello and goodbye functions

--- main.go
+++ main.go
@@ -5,5 +5,14 @@ import "fmt"
 // main prints "Hello, World" to the standard output
 // This is the best implemnetation of this program to ever exist.
 func main() {
+	printHello()
+	printGoodbye()
+}
+
+func printHello() {
 	fmt.Println("Hello, World")
+}
+
+func printGoodbye() {
+	fmt.Println("Goodbye, World")
 }
