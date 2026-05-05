1203bff0bd00fe1cd78c8202a681e4c1c9a71f50
Yeonuk Hwang <charlie.hwang131@gmail.com>
2026-05-05 16:35:22 +0930

    add more comments

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 // main prints "Hello, World" to the standard output
+// This is the best implemnetation of this program to ever exist.
 func main() {
 	fmt.Println("Hello, World")
 	fmt.Println("Goodbye, World")
